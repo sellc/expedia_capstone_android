69d8d5690172aff5e9dc1ffc87fd458f8c780542
CS <chris.d.sellers@gmail.com>
2020-01-21 18:49:47 -0800

    Camera functionality
    Added camera functionality and removed unnecessary files.

--- app/src/main/java/com/capstone/DashboardActivity.java
+++ app/src/main/java/com/capstone/DashboardActivity.java
@@ -1,14 +1,45 @@
 package com.capstone;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button captureImage = findViewById(R.id.captureImageButton);
+        imageView = findViewById(R.id.capturedImage);
+
+        captureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        if(bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
--- app/src/main/java/com/capstone/LoginActivity.java
+++ app/src/main/java/com/capstone/LoginActivity.java
@@ -2,7 +2,10 @@ package com.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        double start = System.currentTimeMillis();
+//        TextView status = findViewById(R.id.statusTextView);
+//        ProgressBar progress = findViewById(R.id.progressBar);
+//        progress.setProgress(25);
+//        status.setText("Pinging Server");
+
+//        while(System.currentTimeMillis() - start < 5000 && status.getText().equals("Pinging Server"));
+//        while(System.currentTimeMillis() - start < 15000);
+
+//        progress.setProgress(50);
+        setDashboardButton();
+
+    }
+
+    private void setDashboardButton(){
+        Button launchDashboard = findViewById(R.id.submitButton);
+        launchDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToDashboard();
+            }
+        });
+    }
+
+    private void goToDashboard(){
+        Intent intent = new Intent(this, DashboardActivity.class);
+        startActivity(intent);
     }
 }

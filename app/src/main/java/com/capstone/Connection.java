24422961717f318e4f559d8fdd9fa9d3849738f2
CS <chris.d.sellers@gmail.com>
2020-01-22 16:22:03 -0800

    Login and Connection Updates
    Login validates credentials. Connection is able to send and receive info. App no longer crashes if the server isn't available.

--- app/src/main/java/com/capstone/Connection.java
+++ app/src/main/java/com/capstone/Connection.java
@@ -1,58 +1,90 @@
 package com.capstone;
 
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
 
-public class Connection extends Thread {
+public class Connection extends Thread{
 
-    private final int port = 35000;
-    private final SocketAddress address = new InetSocketAddress("192.168.1.4", port);
+    private static final int port = 50000;
+    private static final String address = "192.168.0.1";
     private Socket s;
-    private static PrintWriter pw;
+    private PrintWriter pw;
     private BufferedReader br;
-    private boolean newServerResponse;
-    private String serverResponse;
+    private static boolean newServerResponse;
+    private static String serverResponse;
+    private static String userInput;
 
-    public Connection(){
+    public Connection (){
+        userInput = "";
+        serverResponse = "";
+        newServerResponse = false;
+    }
+
+    public void run(){
         try {
-            s = new Socket();
-            while(!s.isConnected()) {
-                s.connect(address, 5000);
+            while (true) {
+                if (!userInput.equals("")) {
+                    if(establishConnection()) {
+                        sendMessage();
+//                        listenForResponse();
+//                        serverResponse = br.readLine();
+//                        newServerResponse = true;
+
+                        pw.close();
+                        br.close();
+                        s.close();
+                    }
+                }
             }
-            pw = new PrintWriter(s.getOutputStream(), true);
-            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        } catch (IOException e) {
+        } catch (IOException e){
+
+        }
+    }
+
+    private boolean establishConnection(){
+        try {
+            s = new Socket(address, port);
+            if(s.isConnected()){
+                pw = new PrintWriter(s.getOutputStream(), true);
+                br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            }
+            return true;
+        } catch (IOException e){
             e.printStackTrace();
         }
+        return false;
     }
 
-    public void run() {
-        while(!s.isClosed()){
-            if(!serverResponse.equals("")){
-                newServerResponse = true;
-            }
+    private void sendMessage(){
+        if (s.isConnected() && pw != null) {
+            pw.println(userInput);
+            userInput = "";
         }
     }
 
-    public static void sendMessage(String message){
-        pw.println(message);
+//    private void listenForResponse(){
+//        try {
+////            while (!hasNewResponse()) {
+////            }
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+//    }
+
+    public static void setUserInput(String input){
+        userInput = input;
     }
 
-    public static boolean checkForNewResponse(){
-//        if(newServerResponse){
-//
-//        }
-        return false;
+    public static boolean hasNewResponse(){
+        return newServerResponse;
+    }
+
+    public static String getServerResponse(){
+        newServerResponse = false;
+        return serverResponse;
     }
 
 }
--- app/src/main/java/com/capstone/LoginActivity.java
+++ app/src/main/java/com/capstone/LoginActivity.java
@@ -6,40 +6,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Connection c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        double start = System.currentTimeMillis();
-//        TextView status = findViewById(R.id.statusTextView);
-//        ProgressBar progress = findViewById(R.id.progressBar);
-//        progress.setProgress(25);
-//        status.setText("Pinging Server");
+        c = new Connection();
+        c.start();
+//        setStatusText("Authenticating...");
 
-//        while(System.currentTimeMillis() - start < 5000 && status.getText().equals("Pinging Server"));
-//        while(System.currentTimeMillis() - start < 15000);
-
-//        progress.setProgress(50);
-        setDashboardButton();
-
+        setSubmitButton();
     }
 
-    private void setDashboardButton(){
+    private void setSubmitButton(){
         Button launchDashboard = findViewById(R.id.submitButton);
         launchDashboard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToDashboard();
+                setStatusText("Authenticating...");
+                EditText username = findViewById(R.id.usernameText);
+                EditText password = findViewById(R.id.passwordText);
+                Connection.setUserInput("{Username:" + username.getText() + "}{Password:" + password.getText()+ "}");
+
+//                while(!Connection.hasNewResponse());
+//                System.out.println(Connection.getServerResponse());
+
+
+//                if(Connection.getServerResponse().equals("1")){
+//                    setStatusText("Login Successful");
+                    goToDashboard();
+//                } else {
+//                    setStatusText("Invalid Credentials");
+//                }
             }
         });
     }
 
     private void goToDashboard(){
+        setStatusText("");
         Intent intent = new Intent(this, DashboardActivity.class);
         startActivity(intent);
+    }
+
+    private void setStatusText(String status){
+        TextView statusText = findViewById(R.id.statusTextView);
+        statusText.setText(status);
     }
 }

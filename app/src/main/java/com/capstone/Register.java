5df3b5d9472ba7589f792fab4e8b88cd7edf2781
sellc <chris.d.sellers@gmail.com>
2020-02-19 08:33:54 -0800

    Refactored Registration Activity
    Condensed logic, removed unnecessary duplications and added a few comments.

--- app/src/main/java/com/capstone/Register.java
+++ app/src/main/java/com/capstone/Register.java
@@ -34,6 +34,7 @@ public class Register extends AppCompatActivity {
         setRegisterButton();
     }
 
+    //Set the register button
     private void setRegisterButton(){
         Button register = findViewById(R.id.submitButton);
         register.setOnClickListener(new View.OnClickListener() {
@@ -44,7 +45,7 @@ public class Register extends AppCompatActivity {
                     public void run() {
                         setStatus("Creating Account...");
                         ProgressBar spinner = findViewById(R.id.registerProgressBar);
-                        spinner.setVisibility(View.VISIBLE);
+                        spinner.setVisibility(View.VISIBLE);    //Display the progress bar for the user
                         spinner.setIndeterminate(true);
                     }
                 });
@@ -53,56 +54,62 @@ public class Register extends AppCompatActivity {
         });
     }
 
+    //Set the back button
     private void setBackButton(){
         Button back = findViewById(R.id.backButton);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                finish();   //End the activity and return to the last activity
             }
         });
     }
 
+    //Start a dashboard activity
     private void goToDashboard(){
         Intent intent = new Intent(this, DashboardActivity.class);
         startActivity(intent);
     }
 
+    //Add a POST request to the queue and try to register a user
     public void register() {
         ra.addPOSTToQueue(Paths.getRegisterPath(), "username="+getUsername()+"&password="+getPassword());
-        Thread check = new Thread(){
+        Thread check = new Thread(){    //Create a new thread so the main thread isn't blocked
             @Override
             public void run() {
                 super.run();
-                checkForToken();
+                checkForResponse();
             }
         };
         check.start();
     }
 
+    //Get the username text
     private String getUsername(){
         EditText username = findViewById(R.id.usernameText);
         return String.valueOf(username.getText());
     }
 
+    //Get the password text
     private String getPassword(){
         EditText password = findViewById(R.id.passwordText);
         return String.valueOf(password.getText());
     }
 
-    private void checkForToken(){
-        String response = ra.getResponse();
+    //Check for a response from the server
+    private void checkForResponse(){
+        String response = ra.getResponse(); //Wait for a response. This is a blocking call in RequestActions thread
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 ProgressBar spinner = findViewById(R.id.registerProgressBar);
-                spinner.setVisibility(View.INVISIBLE);
+                spinner.setVisibility(View.INVISIBLE);  //Hide the spinner
             }
         });
 
-        System.out.println(response);
+//        System.out.println(response);
 
-        if(response.contains("200")){
+        if(response.contains("200")){   //Registration was successful
             ra.addPOSTToQueue(Paths.getLoginPath(), "username="+getUsername()+"&password="+getPassword());
             runOnUiThread(new Runnable() {
                 @Override
@@ -113,50 +120,36 @@ public class Register extends AppCompatActivity {
                 }
             });
             response = ra.getResponse();
-            if(response.contains("token")) {
+            if(response.contains("token")) {    //Login was successful
                 Credentials.setToken(response.substring(response.indexOf("Bearer"), response.length() - 2));
                 goToDashboard();
             }
         } else if(response.contains("Username taken")) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    TextView usernameColon = findViewById(R.id.usernameColon);
-                    TextView passwordColon = findViewById(R.id.passwordColon);
-                    usernameColon.setTextColor(Color.RED);
-                    passwordColon.setTextColor(Color.RED);
-                    setStatus("Username Taken");
-                }
-            });
+            error("Username Taken");
         } else {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    TextView usernameColon = findViewById(R.id.usernameColon);
-                    TextView passwordColon = findViewById(R.id.passwordColon);
-                    usernameColon.setTextColor(Color.RED);
-                    passwordColon.setTextColor(Color.RED);
-                    setStatus("Invalid Credentials");
-                }
-            });
+            error("Invalid Credentials");
         }
     }
 
+    //Call if a request returned a 4xx.
+    // i.e. 400 bad request (username taken)
+    private void error(String reason){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView usernameColon = findViewById(R.id.usernameColon);
+                TextView passwordColon = findViewById(R.id.passwordColon);
+                usernameColon.setTextColor(Color.RED);
+                passwordColon.setTextColor(Color.RED);
+                setStatus(reason);
+            }
+        });
+    }
+
+    //Set the status text
     private void setStatus(String status){
         TextView statusText = findViewById(R.id.statusText);
         statusText.setText(status);
-    }
-
-    public void login() {
-        ra.addPOSTToQueue(Paths.getLoginPath(), "username="+getUsername()+"&password="+getPassword());
-        Thread check = new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                checkForToken();
-            }
-        };
-        check.start();
     }
 
 }

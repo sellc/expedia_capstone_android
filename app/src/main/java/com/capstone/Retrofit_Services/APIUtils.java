1140e661d24fb6e1f2b7c940c77ca8938dc83722
Simone Ray <simray84@gmail.com>
2020-06-10 16:08:28 -0700

    Added callback timeout

--- app/src/main/java/com/capstone/Retrofit_Services/APIUtils.java
+++ app/src/main/java/com/capstone/Retrofit_Services/APIUtils.java
@@ -1,7 +1,7 @@
 package com.capstone.Retrofit_Services;
 
 public class APIUtils {
-    private static final String API_URL="http://ec2-34-220-221-99.us-west-2.compute.amazonaws.com:3001/api/v1/";
+    private static final String API_URL="http://ec2-34-210-193-105.us-west-2.compute.amazonaws.com:3001/api/v1/";
     private static final FileService fileService = RetrofitClient.getClient(API_URL).create(FileService.class);
 
     public static FileService getFileService() {
--- app/src/main/java/com/capstone/Retrofit_Services/RetrofitClient.java
+++ app/src/main/java/com/capstone/Retrofit_Services/RetrofitClient.java
@@ -1,5 +1,7 @@
 package com.capstone.Retrofit_Services;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -8,7 +10,11 @@ public class RetrofitClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(String url){
-        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .build();
+
         if (retrofit == null){
             retrofit = new Retrofit.Builder().baseUrl(url)
                     .addConverterFactory(GsonConverterFactory.create())
